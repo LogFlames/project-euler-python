4665233afb60bc2d59b1133dd876c758eb7e2f93
Elias Lundell <elilun03@gmail.com>
2023-06-03 00:27:08 +0200

    solve 65 - try on 60

--- pe60_nf.py
+++ pe60_nf.py
@@ -1,3 +1,5 @@
+import math
+
 def generate_primes(n):
     n_sqrt = n ** 0.5
     pot_primes = [i for i in range(3, n, 2)]
@@ -22,16 +24,25 @@ def generate_primes(n):
 
     return primes
 
-primes = generate_primes(1000000)
-primes = {*map(str, primes)}
+
+length = 7
+num_matches = 4
+
+primes = generate_primes(10**length)
+primes_check = {*primes}
 
 pp = {}
-for p1 in primes:
-    for p2 in primes:
-        if p1 >= p2:
-            continue
+for i, p1 in enumerate(primes):
+    if i % 1000 == 0:
+        print(f"{i} / {len(primes)}")
 
-        if p1 + p2 in primes and p2 + p1 in primes:
+    if int(math.log10(p1)) == length:
+        break
+    for j, p2 in enumerate(primes):
+        if j >= i:
+            break
+
+        if p1 * 10**(int(math.log10(p2)) + 1) + p2 in primes_check and p2 * 10**(int(math.log10(p1)) + 1) + p1 in primes_check:
             if p1 not in pp:
                 pp[p1] = []
             if p2 not in pp:
@@ -41,13 +52,54 @@ for p1 in primes:
 
 ppp = {}
 
+
+print("Sorting out the ones with too few connections...")
+
 for p in pp:
-    if len(pp[p]) >= 3:
+    if len(pp[p]) >= num_matches - 1:
         ppp[p] = pp[p]
 
-print(ppp)
-print([*ppp])
 
+print("Sorting out the ones with too few connections... Done")
+print("Finding all triangles of primes...")
+trigs = []
+for p in ppp:
+    for p2 in ppp[p]:
+        for p3 in ppp[p]:
+            if p2 == p3:
+                continue
+            if p2 in ppp and p3 in ppp and p3 in ppp[p2] and p2 in ppp[p3]:
+                trigs.append([p, p2, p3])
 
+print("Finding all triangles of primes... Done")
+print("Finding all four-sets of primes...")
 
+fours = []
+for t1 in trigs:
+    for p in ppp:
+        if p in t1:
+            continue
+        if t1[0] in ppp[p] and t1[1] in ppp[p] and t1[2] in ppp[p]:
+            fours.append([p, *t1])
 
+print("Finding all four-sets of primes... Done")
+print("Finding all five-sets of primes...")
+
+fives = []
+for f1 in fours:
+    for p in ppp:
+        if p in f1:
+            continue
+        if f1[0] in ppp[p] and f1[1] in ppp[p] and f1[2] in ppp[p] and f1[3] in ppp[p]:
+            fives.append([p, *f1])
+
+print("Finding all five-sets of primes... Done")
+
+ss = -1
+for f in fives:
+    s = sum(map(int, f))
+    if ss == -1 or s < ss:
+        ss = s
+
+print(ss)
+print(fives)
---
+++ pe65.py
@@ -0,0 +1,70 @@
+import math
+
+class Number:
+    def __init__(self, num, de):
+        self.num = num 
+        self.de = de
+
+    def __add__(self, other):
+        mgm = self.de * other.de // math.gcd(self.de, other.de)
+        return Number(self.num * mgm // self.de + other.num * mgm // other.de, mgm)
+
+    def __floordiv__(self, other):
+        return Number(self.num * other.de, self.de * other.num)
+
+    def minimize(self):
+        g = math.gcd(self.num, self.de)
+        self.de //= g
+        self.num //= g
+
+    def __str__(self):
+        return f"{self.num}/{self.de}"
+
+
+
+def conv_sqrt_2(iters, first = True):
+    if iters == 0:
+        n = Number(0, 1)
+    else:
+        n = conv_sqrt_2(iters - 1, False)
+
+
+    if first:
+        n2 = n + Number(1, 1)
+    else:
+        n2 = Number(1, 1) // (Number(2, 1) + n)
+    return n2
+
+
+def conv_sqrt_23(iters, first = True):
+    if iters == 0:
+        n = Number(0, 1)
+    else:
+        n = conv_sqrt_23(iters - 1, False)
+
+
+    if first:
+        n2 = n + Number(4, 1)
+    else:
+        n2 = Number(1, 1) // (Number([1, 3, 1, 8][(iters + 3) % 4], 1) + n)
+    return n2
+
+def conv_e(iters, first = True, depth = -1):
+    if iters == 0:
+        n = Number(0, 1)
+    else:
+        n = conv_e(iters - 1, first = False, depth = depth + 1)
+
+
+    if first:
+        n2 = n + Number(2, 1)
+    else:
+        if depth % 3 == 0 or depth % 3 == 2:
+            v = 1
+        else:
+            v = 2 * ((depth + 2) // 3)
+        n2 = Number(1, 1) // (Number(v, 1) + n)
+    return n2
+
+n = conv_e(99)
+print(sum(map(int, str(n.num))))
